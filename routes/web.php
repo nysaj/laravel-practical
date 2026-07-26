495c7e56191136450539566d7e044a5548fcde78
NYSAJ <pugpug11@gmail.com>
2026-07-27 01:08:51 +0900

    feat: add post listing page

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use App\Livewire\Settings\TwoFactor;
+use App\Livewire\ShowPosts;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
@@ -25,3 +26,5 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/posts/create', CreatePost::class)->name('posts.create');   
 });
+
+Route::get('/posts', ShowPosts::class)->name('posts');
